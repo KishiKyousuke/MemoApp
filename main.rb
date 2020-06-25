8428a408d9469106cfebbc9677864e20d9f56a5e
岸　恭輔 <kishikyousuke@kishikyousuke.local>
2020-06-26 01:54:48 +0900

    クラスにメソッドを追加

--- main.rb
+++ main.rb
@@ -13,26 +13,39 @@ get "/new-memo" do
 end
 
 class Memo
-  attr_reader :contribution
+  attr_reader :page
   def initialize(id:, title:, content:)
-    @contribution = {id: id, title: title, content:content}
+    @page = {id: id, title: title, content:content}
+  end
+
+  class << self
+    def read(path)
+      file = File.read(path)
+      JSON.load("#{file}")
+    end
+
+    def throw_away(hash, id)
+      hash["memos"].delete_if {|memo| memo["id"] == id}
+    end
+  end
+
+  def add(hash,path)
+    hash["memos"] ||= []
+    hash["memos"].push(page)
+    File.open("#{path}","w") do |file|
+      JSON.dump(hash,file)
+    end
   end
 end
 
 post "/new-memo" do
   @text = params[:text]
   text_lines = @text.split("\n",2)
-  hash = {}
-  File.open("./public/json/storehouse.json","r") do |file|
-   hash =  JSON.load(file)
-  end
+  storage_location_path = "./public/json/storehouse.json"
+  hash = Memo.read(storage_location_path)
   memo_id = hash["memos"].empty? ? 1 : (hash["memos"][-1]["id"] + 1)
   memo = Memo.new(id: memo_id, title:text_lines[0], content: text_lines[1])
-  hash["memos"] ||= []
-  hash["memos"].append(memo.contribution)
-  File.open("./public/json/storehouse.json", "w") do |file|
-    JSON.dump(hash, file)
-  end
+  memo.add(hash,storage_location_path)
   redirect to("/")
 end
 
@@ -52,26 +65,18 @@ patch "/:id" do
   @text = params[:text]
   @id = params[:id]
   text_lines = @text.split("\n",2)
-  hash = {}
-  File.open("./public/json/storehouse.json","r") do |file|
-   hash =  JSON.load(file)
-  end
-  hash["memos"].delete_if {|memo| memo["id"] == @id.to_i}
-  update_memo = Memo.new(id: @id.to_i, title:text_lines[0], content:text_lines[1])
-  hash["memos"].append(update_memo.contribution)
-  File.open("./public/json/storehouse.json", "w") do |file|
-    JSON.dump(hash, file)
-  end
+  storage_location_path = "./public/json/storehouse.json"
+  hash = Memo.read(storage_location_path)
+  Memo.throw_away(hash, @id.to_i)
+  memo = Memo.new(id: @id.to_i, title:text_lines[0], content:text_lines[1])
+  memo.add(hash,storage_location_path)
   redirect to ("/#{@id}")
 end
 
 delete "/:id" do
   @id = params[:id]
-  hash = {}
-  File.open("./public/json/storehouse.json","r") do |file|
-   hash =  JSON.load(file)
-  end
-  hash["memos"].delete_if {|memo| memo["id"] == @id.to_i}
+  hash = Memo.read("./public/json/storehouse.json")
+  Memo.throw_away(hash,@id.to_i)
   File.open("./public/json/storehouse.json", "w") do |file|
     JSON.dump(hash, file)
   end
